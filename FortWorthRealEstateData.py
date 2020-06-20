65cfdb03560201f7d00fc5cb8b6bf4e34991186c
Thomas Burgess <me@thomasburgess.org>
2020-06-20 10:46:28 -0500

    Incorporate pandas dataframe into functions, add printAddressSummary function

--- FortWorthRealEstateData.py
+++ FortWorthRealEstateData.py
@@ -67,6 +67,69 @@ class Property:
     redfinSoldDate = "N/A"
     redfinSqft = "N/A"
 
+    def to_dict(self):
+        return {
+            'RP': self.RP ,
+            'Appraisal_Year': self.Appraisal_Year,
+            'Account_Num': self.Account_Num,
+            'Record_Type': self.Record_Type,
+            'Sequence_No': self.Sequence_No,
+            'PIDN': self.PIDN,
+            'Owner_Name': self.Owner_Name,
+            'Owner_Address': self.Owner_Address,
+            'Owner_CityState': self.Owner_CityState,
+            'Owner_Zip': self.Owner_Zip,
+            'Owner_Zip4': self.Owner_Zip4,
+            'Owner_CRRT': self.Owner_CRRT,
+            'Situs_Address': self.Situs_Address,
+            'Property_Class': self.Property_Class,
+            'TAD_Map': self.TAD_Map,
+            'MAPSCO': self.MAPSCO,
+            'Exemption_Code': self.Exemption_Code,
+            'State_Use_Code': self.State_Use_Code,
+            'LegalDescription': self.LegalDescription,
+            'Notice_Date': self.Notice_Date,
+            'County': self.County,
+            'City': self.City,
+            'School': self.School,
+            'Num_Special_Dist': self.Num_Special_Dist,
+            'Spec1': self.Spec1,
+            'Spec2': self.Spec2,
+            'Spec3': self.Spec3,
+            'Spec4': self.Spec4,
+            'Spec5': self.Spec5,
+            'Deed_Date': self.Deed_Date,
+            'Deed_Book': self.Deed_Book,
+            'Deed_Page': self.Deed_Page,
+            'Land_Value': self.Land_Value,
+            'Improvement_Value': self.Improvement_Value,
+            'Total_Value': self.Total_Value,
+            'Garage_Capacity': self.Garage_Capacity,
+            'Num_Bedrooms': self.Num_Bedrooms,
+            'Num_Bathrooms': self.Num_Bathrooms,
+            'Year_Built': self.Year_Built,
+            'Living_Area': self.Living_Area,
+            'Swimming_Pool_Ind': self.Swimming_Pool_Ind,
+            'ARB_Indicator': self.ARB_Indicator,
+            'Ag_Code': self.Ag_Code,
+            'Land_Acres': self.Land_Acres,
+            'Land_SqFt': self.Land_SqFt,
+            'Ag_Acres': self.Ag_Acres,
+            'Ag_Value': self.Ag_Value,
+            'Central_Heat_Ind': self.Central_Heat_Ind,
+            'Central_Air_Ind': self.Central_Air_Ind,
+            'Structure_Count': self.Structure_Count,
+            'From_Accts': self.From_Accts,
+            'Appraisal_Date': self.Appraisal_Date,
+            'Appraised_Value': self.Appraised_Value,
+            'GIS_Link': self.GIS_Link,
+            'Instrument_No': self.Instrument_No,
+            'Overlap_Flag': self.Overlap_Flag,
+            'redfinSoldPrice': self.redfinSoldPrice,
+            'redfinSoldDate': self.redfinSoldDate,
+            'redfinSqft': self.redfinSqft
+        }
+
 class redfinProperty:
     def __init__(self,address,price,date,sqft):
         self.address = address
@@ -99,6 +162,7 @@ def loadProperties():
                 line_count += 1
                 if (property_count%100000==0):
                     print (str(property_count)+ " properties loaded.\n")
+        makePropertyFrame()
         print("\nDone!\n")
 
 #Exports properties loaded from CSV (and any data merged from Redfin or other sources) to pickle
@@ -112,6 +176,7 @@ def importPickle():
     global properties
     with open ('property.pickle', 'rb') as f:
         properties=pickle.load(f)
+    makePropertyFrame()
     print("\nDone!\n")
 
 #Prints every address and market value (from TAD)
@@ -124,23 +189,29 @@ def printAllValues():
 
 #Prints all data on given address
 def printAddressData(address):
-    property_count=0
-    global properties
-    for lot in properties:
-        if address.upper() in properties[property_count].Situs_Address:
-            print ("\nAddress [TAD]: " + properties[property_count].Situs_Address)
-            print ("Market Value [TAD]: " + properties[property_count].Total_Value)
-            print ("Appraisal Year [TAD]: " + properties[property_count].Appraisal_Year)
-            print ("Owner Name [TAD]: " + properties[property_count].Owner_Name)
-            print ("Owner Address [TAD]: " + properties[property_count].Owner_Address + ", " + properties[property_count].Owner_Zip)
-            print ("Property Class [TAD]: " + properties[property_count].Property_Class)
-            print ("Deed Date [TAD]: " + properties[property_count].Deed_Date)
-            print ("Acres [TAD]: " + properties[property_count].Land_Acres)
-            print ("Sqft [Redfin]: " + properties[property_count].redfinSqft)
-            print ("Last sold price [Redfin]: " + properties[property_count].redfinSoldPrice)
-            print ("Last sold date [Redfin]: " + properties[property_count].redfinSoldDate + "\n")
-        property_count += 1
-    print ("\nDone!\n")
+    # property_count=0
+    # global properties
+    # for lot in properties:
+    #     if address.upper() in properties[property_count].Situs_Address:
+    #         print ("\nAddress [TAD]: " + properties[property_count].Situs_Address)
+    #         print ("Market Value [TAD]: " + properties[property_count].Total_Value)
+    #         print ("Appraisal Year [TAD]: " + properties[property_count].Appraisal_Year)
+    #         print ("Owner Name [TAD]: " + properties[property_count].Owner_Name)
+    #         print ("Owner Address [TAD]: " + properties[property_count].Owner_Address + ", " + properties[property_count].Owner_Zip)
+    #         print ("Property Class [TAD]: " + properties[property_count].Property_Class)
+    #         print ("Deed Date [TAD]: " + properties[property_count].Deed_Date)
+    #         print ("Acres [TAD]: " + properties[property_count].Land_Acres)
+    #         print ("Sqft [Redfin]: " + properties[property_count].redfinSqft)
+    #         print ("Last sold price [Redfin]: " + properties[property_count].redfinSoldPrice)
+    #         print ("Last sold date [Redfin]: " + properties[property_count].redfinSoldDate + "\n")
+    #     property_count += 1
+    # This is much more efficient lol
+    property_row = propertyFrame.loc[address.upper(),:]
+    print (property_row)
+
+def printAddressSummary(address):
+    property_row = propertyFrame.loc[address.upper(),['Total_Value', 'Appraisal_Year','Owner_Name','Owner_Address','Owner_Zip','Property_Class','Deed_Date','Land_Acres','redfinSqft','redfinSoldPrice','redfinSoldDate']]
+    print (property_row)
 
 #Adds the prices of the homes sold in the last year to the corresponding property object. Data from Redfin.
 def redfinImport():
@@ -168,33 +239,53 @@ def redfinImport():
                 #print (redfinProperties[redfin_count].address + " found, it is worth " + str(properties[property_count].Total_Value+" and sold for "+str(redfinProperties[redfin_count].price)+" on " + properties[property_count].redfinSoldDate+ "\n"))
             property_count += 1
         redfin_count += 1
+    makePropertyFrame()
     print("\nDone!\n")
+
+def makePropertyFrame():
+    global propertyFrame
+    propertyFrame = pd.DataFrame(p.to_dict() for p in properties)
+    propertyFrame = propertyFrame.set_index('Situs_Address')
+    propertyFrame.index.names = [None]
+
 
 #Menu
 #SET WORKING DIRECTORY HERE
 os.chdir('C:/Users/Thomas/Desktop/FortWorthRealEstateData')
 print ("Working directory is: " + os.getcwd())
-#print ("Contents of directory: " + str(os.listdir()))
+
 while (userChoice!='0'):
-    print ("\nSelect your choice from the menu (e.g. '2'):\n1: Load properties into program from TAD CSV\n2: Load properties into program from pickle\n3: Export properties to pickle\n4: Print all data on address\n5: Merge Redfin file with TAD data\n0: Quit program\n")
+    print ("\nSelect your choice from the menu (e.g. '2'):\n1: Load properties into program from previously saved pickle\n2: Load properties into program from TAD CSV\n3: Merge Redfin file with TAD data\n4: Export properties to pickle\n0: Done manipulating data, move on to next menu.\n")
     userChoice=input()
     if (userChoice=='1'):
-        print ("\nLoading properties from CSV...\n")
-        loadProperties()
-    elif (userChoice=='2'):
         print ("\nLoading properties from pickle...\n")
         importPickle()
+    elif (userChoice=='2'):
+        print ("\nLoading properties from CSV...\n")
+        loadProperties()
     elif (userChoice=='3'):
-        print ("\nExporting properties to pickle...\n")
-        exportPickle()
-    elif (userChoice=='4'):
-        print ("\nPlease enter address to search: \n")
-        searchTerm=input()
-        printAddressData(searchTerm)
-    elif (userChoice=='5'):
         print ("Merging Redfin data with TAD data...")
         redfinImport()
+    elif (userChoice=='4'):
+        print ("\nExporting properties to pickle...\n")
+        exportPickle()
 
+    elif (userChoice=='0'):
+        print ("\n")
+    else:
+        print("You entered: " + userChoice + ". That was not a valid option, please try again.\n")
+userChoice = 1
+
+
+while (userChoice!='0'):
+    print("\nSelect your choice from the menu (e.g. '2'):\n1: Print all data on an address.\n0: Quit program.\n")
+    userChoice=input()
+    if (userChoice=='1'):
+        print("\nPlease enter address to search: \n")
+        searchTerm=input()
+        printAddressData(searchTerm)
+    elif (userChoice=='2'):
+        print (propertyFrame.head(5))
     elif (userChoice=='0'):
         print ("Goodbye!\n")
     else:
